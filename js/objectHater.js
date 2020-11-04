92536feb218a774784cdb1b69088be0ee60b77b0
Pablo Fernandez <pablo_fz@hotmail.com>
2020-11-04 11:15:32 +0100

    movement hater fixed

--- js/objectHater.js
+++ js/objectHater.js
@@ -21,8 +21,14 @@ class Hater {
   update() {
     this.x = this.x + this.directionX * this.speed;
     this.y = this.y + this.directionY * this.speed;
-    if(!this.onObjectiveX)this.setDirectionX();
-    if(!this.onObjectiveY)this.setDirectionY();
+    if(!this.onObjectiveX){
+      this.setDirectionX();
+      
+    }
+    if(!this.onObjectiveY){
+      this.setDirectionY();
+     
+    }
   }
 
   draw() {
@@ -31,18 +37,29 @@ class Hater {
   }
 
   setDirectionX() { 
+
+    // const proportion = (Math.abs(this.x - this.objectiveX)/Math.abs(this.y - this.objectiveY));
+
+    // this.directionX = 1*proportion;
+    // this.directionY = 1/proportion;
   
     if((this.x - this.objectiveX)<0) this.directionX = 1;
     else if ((this.x - this.objectiveX)>0) this.directionX = -1;
-    else this.onObjectiveX = true;
+    else {
+      this.onObjectiveX = true;
+      if (!this.onObjectiveY)this.directionX =0;
+    }
 
-  }
+}
 
   setDirectionY() { 
   
     if((this.y - this.objectiveY)<0) this.directionY = 1;
     else if ((this.y - this.objectiveY)>0) this.directionY = -1;
-    else this.onObjectiveY = true;
+    else {
+      this.onObjectiveY = true;
+      if (!this.onObjectiveX)this.directionY =0;
+    }
   }
 
 
